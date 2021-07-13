fbf1e482f0c34094ddda3ee93540939af176eab2
Oyinkansola89 <78278350+Oyinkansola89@users.noreply.github.com>
2021-07-13 18:41:01 +0100

    attempted to update unit conversion

--- src/app.js
+++ src/app.js
@@ -1,5 +1,16 @@
 
 function formatDate(date) {
+let date = new Date();
+
+let currentHour =  date.getHours();
+if (currentHour < 10 ){
+  currentHour = `0${currentHour}`;
+}
+let currentMinutes = date.getMinutes();
+if (currentMinutes < 10 ){
+  currentMinutes = `0${currentMinutes}`;
+}
+
 let days = [
   "Sunday",
   "Monday",
@@ -11,30 +22,16 @@ let days = [
 ];
 
 let currentDay = days[date.getDay()];
-let currentHour =  date.getHours();
-if (currentHour < 10 ){
-  currentHour = `0${currentHour}`;
-}
-let currentMinutes = date.getMinutes();
-if (currentMinutes < 10 ){
-  currentMinutes = `0${currentMinutes}`;
-}
-
 return `${currentDay}, ${currentHour}:${currentMinutes}`;                                                                             
 }
 
-let currentTime = new Date();
-let formattedDate = document.querySelector("#time");
-formattedDate.innerHTML= formatDate(currentTime);       
 
-
-
+    
 function displayWeatherCondition(response) {
   let city = document.querySelector("#city");
   city.innerHTML = response.data.name;
   let temp = document.querySelector("#units");
-  temp.innerHTML = Math.round(response.data.main.temp
-  );
+  temp.innerHTML = Math.round(celsiusTemperature);
 
   let description = document.querySelector("#description");
   description.innerHTML =response.data.weather[0].main;                
@@ -44,6 +41,12 @@ function displayWeatherCondition(response) {
 
   let wind = document.querySelector("#wind");
   wind.innerHTML = response.data.wind.speed;
+
+
+ let formattedDate = document.querySelector("#time");
+ formattedDate.innerHTML= formatDate(response.data.dt * 1000); //format date call
+
+ celsiusTemperature = response.data.main.temp;
 
 }
 
@@ -55,20 +58,46 @@ function searchCity(city) {
   axios.get(apiUrl).then(displayWeatherCondition); 
 }
 
-searchCity("New York");    
-
-
 function handleSubmit(event) {
   event.preventDefault();
-  let city = document.querySelector("#search-text-input").value;
- 
-  searchCity(city);  
+  let cityInput = document.querySelector("#search-text-input");
+  
+  searchCity(cityInput.value);  
  
 }
+
+function displayFahrenheitTemperature(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#units");
+
+  celsiusLink.classList.remove("active");
+  fahrenheitLink.classList.add("active");
+  let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
+  temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
+}
+
+function displayCelsiusTemperature(event) {
+  event.preventDefault();
+  celsiusLink.classList.add("active");
+  fahrenheitLink.classList.remove("active");
+  let temperatureElement = document.querySelector("#units");
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
+}
+
+let celsiusTemperature = null;
+
 
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", handleSubmit);
 
+
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
+
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", displayCelsiusTemperature);
+
+searchCity("New York");    
 
 
 function searchLocation(position) {
@@ -88,6 +117,4 @@ function getCurrentLocation(event) {
 
 let currentLocationButton = document.querySelector("#current-location-button");          
 currentLocationButton.addEventListener("click", getCurrentLocation);
-
-
 
